f4f3e019b0c49e9f56f319953e3e79f9637374b3
Gregory Wagner <wagner.greg@gmail.com>
2023-05-23 13:22:41 -0400

    Tighter priors

--- src/parameter_sets.jl
+++ src/parameter_sets.jl
@@ -47,22 +47,22 @@ dependent_parameter_sets["fixed_Ric"] = (; CˡᵒD = CˡᵒD_fixed_Riᶜ)
 bounds_library = Dict()
 
 # Turbulent kinetic energy parameters
-bounds_library[:CᵂwΔ] = (0.0, 16.0)
-bounds_library[:Cᵂu★] = (0.0, 16.0)
+bounds_library[:CᵂwΔ] = (0.0, 8.0)
+bounds_library[:Cᵂu★] = (0.0, 8.0)
 
 # Mixing length parameters
 bounds_library[:Cˢ]   = (0.0, 4.0)
 bounds_library[:Cᵇ]   = (0.0, 1.0)
 
-bounds_library[:CˡᵒD] = (0.0, 8.0)
-bounds_library[:Cˡᵒu] = (0.0, 2.0)
-bounds_library[:Cˡᵒc] = (0.0, 2.0)
-bounds_library[:Cˡᵒe] = (0.0, 2.0)
+bounds_library[:CˡᵒD] = (0.0, 4.0)
+bounds_library[:Cˡᵒu] = (0.0, 4.0)
+bounds_library[:Cˡᵒc] = (0.0, 4.0)
+bounds_library[:Cˡᵒe] = (0.0, 4.0)
 
-bounds_library[:Cʰⁱc] = (0.0, 8.0)
-bounds_library[:Cʰⁱu] = (0.0, 2.0)
-bounds_library[:CʰⁱD] = (0.0, 2.0)
-bounds_library[:Cʰⁱe] = (0.0, 2.0)
+bounds_library[:Cʰⁱc] = (0.0, 4.0)
+bounds_library[:Cʰⁱu] = (0.0, 4.0)
+bounds_library[:CʰⁱD] = (0.0, 4.0)
+bounds_library[:Cʰⁱe] = (0.0, 4.0)
 
 bounds_library[:CRi⁰] = (0.0, 1.0)
 bounds_library[:CRiᵟ] = (0.0, 1.0)
@@ -74,7 +74,7 @@ bounds_library[:CᶜD]  = (0.0, 4.0)
 bounds_library[:Cᵉc]  = (0.0, 1.0)
 bounds_library[:Cᵉe]  = (0.0, 1.0)
 bounds_library[:CᵉD]  = (0.0, 1.0)
-bounds_library[:Cˢᵖ]  = (0.0, 2.0)
+bounds_library[:Cˢᵖ]  = (0.0, 4.0)
 
 # Ri-based
 bounds_library[:ν₀]  = (0.0, 1.0)
