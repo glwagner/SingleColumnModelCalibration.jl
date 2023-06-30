db472f739b1c11519752d296eec2a69413a8b125
adelinehillier <adelineh2000@gmail.com>
2023-06-30 15:37:12 -0400

    Update parameters and bounds to recent CATKE

--- src/parameter_sets.jl
+++ src/parameter_sets.jl
@@ -1,3 +1,5 @@
+using LaTeXStrings
+
 parameter_sets = Dict(
     "ri_based"    => (:ν₀, :κ₀, :κᶜᵃ, :Cᵉⁿ, :Cᵃᵛ, :Ri₀, :Riᵟ),
     "constant_Pr" => (:CᵂwΔ, :Cᵂu★, :Cʰⁱc, :Cʰⁱu, :Cʰⁱe, :CʰⁱD, :Cˢ),
@@ -40,87 +42,49 @@ end
 
 dependent_parameter_sets["fixed_Ric"] = (; CˡᵒD = CˡᵒD_fixed_Riᶜ)
 
-parameter_guide = Dict(:C⁻D    => (name = "Dissipation parameter (TKE equation)", latex = L"C^D", bounds = (0.1, 10.0)), 
-                        :C⁺D    => (name = "Dissipation parameter (TKE equation)", latex = L"C^D", bounds = (0.1, 10.0)), 
-                        # :Cᵟu   => (name = "Ratio of mixing length to grid spacing", latex = L"C^{\delta}u" bounds = (0.0, 4.0)), 
-                        # :Cᵟc   => (name = "Ratio of mixing length to grid spacing", latex = L"C^{\delta}c" bounds = (0.0, 4.0)), 
-                        # :Cᵟe   => (name = "Ratio of mixing length to grid spacing", latex = L"C^{\delta}e" bounds = (0.0, 4.0)), 
-                        :Cᵂu★  => (name = "TKE subgrid flux parameter", latex = L"C^W_{u\star}", bounds = (0.1,  10.0)), 
-                        :CᵂwΔ  => (name = "TKE subgrid flux parameter", latex = L"C^Ww\Delta", bounds = (0.1, 10.0)), 
-                        :CRiʷ => (name = "Stability function parameter", latex = L"CRi^w", bounds = (0.1, 2.0)), 
-                        :CRiᶜ => (name = "Stability function parameter", latex = L"CRi^c", bounds = (0.1, 1.0)), 
-                        :C⁻u  => (name = "Velocity diffusivity LB", latex = L"C^Ku^-", bounds = (0.1, 1.0)), 
-                        :C⁺u  => (name = "Velocity diffusivity (UB-LB)/LB", latex = L"C^Ku^r", bounds = (0.1, 1.0)), 
-                        :C⁻c  => (name = "Tracer diffusivity LB", latex = L"C^Kc^-", bounds = (0.1, 1.0)), 
-                        :C⁺c  => (name = "Tracer diffusivity (UB-LB)/LB", latex = L"C^Kc^r", bounds = (0.1, 1.0)), 
-                        :C⁻e  => (name = "TKE diffusivity LB", latex = L"C^Ke^-", bounds = (0.1, 10.0)), 
-                        :C⁺e  => (name = "TKE diffusivity (UB-LB)/LB", latex = L"C^Ke^r", bounds = (0.1, 10.0)), 
-                        # :Cᴬu   => (name = "Convective mixing length parameter", latex = L"C^A_U", bounds = (0.0, 10.0)), 
-                        # :Cᴬc   => (name = "Convective mixing length parameter", latex = L"C^A_C", bounds = (0.0, 5.0)), 
-                        # :Cᴬe   => (name = "Convective mixing length parameter", latex = L"C^A_E", bounds = (0.0, 30.0)),
-                        :Cᵇ   => (name = "Stratified mixing length parameter", latex = L"C^b_U", bounds = (0.2, 2.0)),
-                        # :Cᵇu   => (name = "Stratified mixing length parameter", latex = L"C^b_U", bounds = (0.0, 2.0)),
-                        # :Cᵇc   => (name = "Stratified mixing length parameter", latex = L"C^b_C", bounds = (0.0, 2.0)),
-                        # :Cᵇe   => (name = "Stratified mixing length parameter", latex = L"C^b_E", bounds = (0.0, 2.0)),
-                        :Cˢ   => (name = "Shear mixing length coefficient", latex = L"C^s_U", bounds = (0.2, 2.0)),
-                        # :Cˢu   => (name = "Shear mixing length coefficient", latex = L"C^s_U", bounds = (0.0, 3.0)),
-                        # :Cˢc   => (name = "Shear mixing length coefficient", latex = L"C^s_C", bounds = (0.0, 3.0)),
-                        # :Cˢe   => (name = "Shear mixing length coefficient", latex = L"C^s_E", bounds = (0.0, 3.0)),
-                        # :Cʷ★   => (name = "Softmin strength parameter", latex = L"C^w_\star", bounds = (1.0, 4.0)),
-                        # :Cʷℓ   => (name = "Softmax strength parameter", latex = L"C^w_\ell", bounds = (0.0, 4.0)),
-                        :Cᶜc => (latex = L"C^c_C", bounds = (0.01, 10.0)),
-                        :Cᶜe => (latex = L"C^c_E", bounds = (0.01, 10.0)),
-                        :CᶜD => (latex = L"C^c_D", bounds = (0.01, 10.0)),
-                        :Cᵉc => (latex = L"C^e_C", bounds = (0.01, 2.0)),
-                        :Cᵉe => (latex = L"C^e_E", bounds = (0.01, 2.0)),
-                        :CᵉD => (latex = L"C^e_D", bounds = (0.01, 2.0)),
-                        :Cˢᶜ => (latex = L"C^{sc}", bounds = (0.01, 10.0)),
+
+parameter_guide = Dict(:CˡᵒD    => (name = "Dissipation parameter (TKE equation)", latex = L"C^{lo}_D", bounds = (0.1, 10.0)), 
+                        :CʰⁱD    => (name = "Dissipation parameter (TKE equation)", latex = L"C^{hi}_D", bounds = (0.1, 10.0)), 
+
+                        :Cᵂu★  => (name = "TKE subgrid flux parameter", latex = L"C^W_{u\star}", bounds = (0.0,  4.0)), 
+                        :CᵂwΔ  => (name = "TKE subgrid flux parameter", latex = L"C^Ww\Delta", bounds = (0.0, 4.0)), 
+
+                        :CRiᵟ => (name = "Stability function parameter", latex = L"CRi^{\delta}", bounds = (0.1, 1.0)), 
+                        :CRi⁰ => (name = "Stability function parameter", latex = L"CRi^{\theta}", bounds = (0.1, 1.0)), 
+
+                        :Cˡᵒu  => (name = "Velocity diffusivity LB", latex = L"C^{lo}u", bounds = (0.1, 1.0)), 
+                        :Cʰⁱu  => (name = "Velocity diffusivity (UB-LB)/LB", latex = L"C^{hi}_u", bounds = (0.1, 1.0)), 
+                        :Cˡᵒc  => (name = "Tracer diffusivity LB", latex = L"C^{lo}_c", bounds = (0.1, 1.0)), 
+                        :Cʰⁱc  => (name = "Tracer diffusivity (UB-LB)/LB", latex = L"C^{hi}_c", bounds = (0.1, 1.0)), 
+                        :Cˡᵒe  => (name = "TKE diffusivity LB", latex = L"C^{lo}_e", bounds = (0.1, 4.0)), 
+                        :Cʰⁱe  => (name = "TKE diffusivity (UB-LB)/LB", latex = L"C^{hi}_e", bounds = (0.1, 4.0)), 
+
+                        :Cᵇ   => (name = "Stratified mixing length parameter", latex = L"C^b", bounds = (0.0, 1.0)),
+                        :Cˢ   => (name = "Shear mixing length coefficient", latex = L"C^s", bounds = (0.0, 1.0)),
+
+                        :Cᶜc => (name = "Convective adjustment parameter", latex = L"C^c_c", bounds = (0.01, 1.0)),
+                        :Cᶜe => (name = "Convective adjustment parameter", latex = L"C^c_e", bounds = (0.01, 1.0)),
+                        :CᶜD => (name = "Convective adjustment parameter", latex = L"C^c_D", bounds = (0.01, 2.0)),
+                        :Cᵉc => (name = "Convective adjustment parameter", latex = L"C^e_c", bounds = (0.01, 1.0)),
+                        :Cᵉe => (name = "Convective adjustment parameter", latex = L"C^e_e", bounds = (0.01, 1.0)),
+                        :CᵉD => (name = "Convective adjustment parameter", latex = L"C^e_D", bounds = (0.01, 1.0)),
+                        :Cˢᵖ => (name = "Convective adjustment parameter", latex = L"C^{sp}", bounds = (0.01, 1.0)),
 )
 
-#####
-##### Bounds and priors
-#####
-
-bounds_library = Dict()
-
-# Turbulent kinetic energy parameters
-bounds_library[:CᵂwΔ] = (0.0, 4.0)
-bounds_library[:Cᵂu★] = (0.0, 4.0)
-
-# Mixing length parameters
-bounds_library[:Cˢ]   = (0.0, 1.0)
-bounds_library[:Cᵇ]   = (0.0, 1.0)
-
-bounds_library[:CˡᵒD] = (0.0, 1.0)
-bounds_library[:Cˡᵒu] = (0.0, 1.0)
-bounds_library[:Cˡᵒc] = (0.0, 1.0)
-bounds_library[:Cˡᵒe] = (0.0, 4.0)
-
-bounds_library[:Cʰⁱc] = (0.0, 1.0)
-bounds_library[:Cʰⁱu] = (0.0, 1.0)
-bounds_library[:CʰⁱD] = (0.0, 1.0)
-bounds_library[:Cʰⁱe] = (0.0, 4.0)
-
-bounds_library[:CRi⁰] = (0.0, 1.0)
-bounds_library[:CRiᵟ] = (0.0, 1.0)
-
-# Convective adjustment parameters
-bounds_library[:Cᶜc]  = (0.0, 1.0)
-bounds_library[:Cᶜe]  = (0.0, 1.0)
-bounds_library[:CᶜD]  = (0.0, 2.0)
-bounds_library[:Cᵉc]  = (0.0, 1.0)
-bounds_library[:Cᵉe]  = (0.0, 1.0)
-bounds_library[:CᵉD]  = (0.0, 1.0)
-bounds_library[:Cˢᵖ]  = (0.0, 1.0)
-
 # Ri-based
-bounds_library[:ν₀]  = (0.0, 1.0)
-bounds_library[:κ₀]  = (0.0, 1.0)
-bounds_library[:κᶜᵃ] = (0.0, 2.0)
-bounds_library[:Cᵉⁿ] = (0.0, 2.0)
-bounds_library[:Cᵃᵛ] = (0.0, 2.0)
-bounds_library[:Ri₀] = (0.0, 2.0)
-bounds_library[:Riᵟ] = (0.0, 2.0)
+# bounds_library[:ν₀]  = (0.0, 1.0)
+# bounds_library[:κ₀]  = (0.0, 1.0)
+# bounds_library[:κᶜᵃ] = (0.0, 2.0)
+# bounds_library[:Cᵉⁿ] = (0.0, 2.0)
+# bounds_library[:Cᵃᵛ] = (0.0, 2.0)
+# bounds_library[:Ri₀] = (0.0, 2.0)
+# bounds_library[:Riᵟ] = (0.0, 2.0)
+
+# prior_library = Dict()
+
+# for p in keys(bounds_library)
+#     prior_library[p] = ScaledLogitNormal(; bounds=bounds_library[p])
+# end
 
 bounds(name) = parameter_guide[name].bounds
 
