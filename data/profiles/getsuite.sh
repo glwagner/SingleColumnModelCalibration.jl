08aebb3ea763b99e08480662ccc213aa62ef41bd
Gregory Wagner <wagner.greg@gmail.com>
2024-06-05 11:34:12 -0400

    Update script to copy over the data

--- data/profiles/getsuite.sh
+++ data/profiles/getsuite.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env bash
+
+shopt -s extglob
+
 tartarusdir='/home/greg/Projects/LESbrary.jl/idealized/'
 basename='three_layer_constant_fluxes_linear_hr'
-hours=$1
+
+hours=6
 suite="${hours}_hour_suite"
 
 mkdir $suite
@@ -8,7 +13,54 @@ mkdir $suite/1m
 mkdir $suite/2m
 mkdir $suite/4m
 
-scp -r tartarus:"${tartarusdir}${basename}${hours}*256*/*statistics.jld2" $suite/1m
-scp -r tartarus:"${tartarusdir}${basename}${hours}*128*/*statistics.jld2" $suite/2m
-scp -r tartarus:"${tartarusdir}${basename}${hours}*64*/*statistics.jld2"  $suite/4m
+cp -r ${tartarusdir}${basename}${hours}*256*/*statistics.jld2 $suite/1m/ 
+cp -r ${tartarusdir}${basename}${hours}*128*/*statistics.jld2 $suite/2m/
+cp -r ${tartarusdir}${basename}${hours}*64*/*statistics.jld2  $suite/4m/
 
+hours=12
+suite="${hours}_hour_suite"
+
+mkdir $suite
+mkdir $suite/1m
+mkdir $suite/2m
+mkdir $suite/4m
+
+cp -r ${tartarusdir}${basename}${hours}*256*/*statistics.jld2 $suite/1m/ 
+cp -r ${tartarusdir}${basename}${hours}*128*/*statistics.jld2 $suite/2m/
+cp -r ${tartarusdir}${basename}${hours}*64*/*statistics.jld2  $suite/4m/
+
+hours=24
+suite="${hours}_hour_suite"
+
+mkdir $suite
+mkdir $suite/1m
+mkdir $suite/2m
+mkdir $suite/4m
+
+cp -r ${tartarusdir}${basename}${hours}*256*/*statistics.jld2 $suite/1m/ 
+cp -r ${tartarusdir}${basename}${hours}*128*/*statistics.jld2 $suite/2m/
+cp -r ${tartarusdir}${basename}${hours}*64*/*statistics.jld2  $suite/4m/
+
+hours=48
+suite="${hours}_hour_suite"
+
+mkdir $suite
+mkdir $suite/1m
+mkdir $suite/2m
+mkdir $suite/4m
+
+cp -r ${tartarusdir}${basename}${hours}*256*/*statistics.jld2 $suite/1m/ 
+cp -r ${tartarusdir}${basename}${hours}*128*/*statistics.jld2 $suite/2m/
+cp -r ${tartarusdir}${basename}${hours}*64*/*statistics.jld2  $suite/4m/
+
+hours=72
+suite="${hours}_hour_suite"
+
+mkdir $suite
+mkdir $suite/1m
+mkdir $suite/2m
+mkdir $suite/4m
+
+cp -r ${tartarusdir}${basename}${hours}*256*/*statistics.jld2 $suite/1m/ 
+cp -r ${tartarusdir}${basename}${hours}*128*/*statistics.jld2 $suite/2m/
+cp -r ${tartarusdir}${basename}${hours}*64*/*statistics.jld2  $suite/4m/
